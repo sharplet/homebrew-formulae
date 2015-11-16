e665231cf87c6f5c04b3cdab356ed54dcc129aa5
Adam Sharp <adsharp@me.com>
2015-11-16 11:59:02 +1100

    todogrep 0.1.2

--- Formula/todogrep.rb
+++ Formula/todogrep.rb
@@ -1,9 +1,9 @@
 class Todogrep < Formula
   desc "Find TODO and FIXME directives in the current directory."
   homepage "https://github.com/sharplet/todogrep"
-  url "https://github.com/sharplet/todogrep/archive/v0.1.1.tar.gz"
-  version "0.1.1"
-  sha256 "b14f5b82f8b2ef3761f4d7d359c4787f4464f808d512b2344cc5b5706141321f"
+  url "https://github.com/sharplet/todogrep/archive/v0.1.2.tar.gz"
+  version "0.1.2"
+  sha256 "3fd812f93e7ef7228fc3d55e5753e97e91e16dff75cf1eee03f7cf76890dbddd"
 
   def install
     bin.install "todogrep"
