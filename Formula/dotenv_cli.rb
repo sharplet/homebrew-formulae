66927d14a32900389327ee7774bac579a0ced9de
Adam Sharp <adam@sharplet.me>
2018-02-19 10:44:22 -0500

    dotenv_cli 0.2.1

--- Formula/dotenv_cli.rb
+++ Formula/dotenv_cli.rb
@@ -1,8 +1,8 @@
 class DotenvCli < Formula
   desc "A CLI for rust-dotenv."
   homepage "https://github.com/sharplet/dotenv_cli"
-  url "https://github.com/sharplet/dotenv_cli/archive/v0.2.0.tar.gz"
-  sha256 "ec9ca9f953faadd17b7635a42555a22dec68eca19dd6ee0cbfa964494baa3c19"
+  url "https://github.com/sharplet/dotenv_cli/archive/v0.2.1.tar.gz"
+  sha256 "abfd8d0f525a2839c6195c385eb253c7d7f60b6b5640f136d72e8bcaf2f49e46"
   depends_on "rust" => :build
 
   def install
