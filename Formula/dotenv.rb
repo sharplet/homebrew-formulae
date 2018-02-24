3587ff144f9941ba5072a93b764e5fd4da14eef2
Adam Sharp <adam@sharplet.me>
2018-02-24 12:09:05 -0500

    Replace dotenv_cli with dotenv (devel, head)
    https://github.com/purpliminal/rust-dotenv/pull/81

---
+++ Formula/dotenv.rb
@@ -0,0 +1,20 @@
+class Dotenv < Formula
+  desc "A `dotenv` implementation for Rust."
+  homepage "https://github.com/purpliminal/rust-dotenv"
+  depends_on "rust" => :build
+
+  devel do
+    url "https://github.com/sharplet/rust-dotenv.git", branch: "dotenv-cli"
+  end
+
+  head "https://github.com/purpliminal/rust-dotenv.git"
+
+  def install
+    system "cargo", "install", "--path", "dotenv_cli", "--root", prefix
+  end
+
+  test do
+     (testpath/".env").write('TEST="hello world"')
+     assert_equal "hello world", shell_output("#{bin}/dotenv /bin/sh -c 'echo $TEST'").strip
+  end
+end
